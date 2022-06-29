030b1a23c9e5989c43cb5eff65c2bb419c09ba63
Kevin Nause <kevin@nause.engineering>
2022-06-28 19:40:21 -0700

    x86 runtime planning

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,2 +1,5 @@
 #[cfg(all(any(target_arch = "riscv32", target_arch = "riscv64"), target_os = "none"))]
 pub mod riscv;
+
+#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_os = "none"))]
+pub mod x86;
